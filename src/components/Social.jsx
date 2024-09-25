906d402be24154abaa889eeb4bc4a19698f627a4
htmcharles <hatumacharles1@gmail.com>
2024-09-25 11:34:28 +0200

    accounts

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,40 +2,47 @@ import { FaTwitter, FaLinkedin, FaInstagram, FaGithub, FaEnvelope, FaWhatsapp } 
 
 const SocialMedia = () => {
   return (
-    <div className="fixed bottom-20 left-10 transform -translate-y-1/2">
+    <div className="fixed top-3/4 left-10 transform -translate-y-1/2">
       <ul className="space-y-3">
         <li>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/htmcharles" target="_blank" rel="noopener noreferrer">
             <div className="bg-[#333] rounded-full p-2">
               <FaGithub className="text-white text-xl" />
             </div>
           </a>
         </li>
         <li>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/hatuma-charles-ba093b302/" target="_blank" rel="noopener noreferrer">
             <div className="bg-[#0077b5] rounded-full p-2">
               <FaLinkedin className="text-white text-xl" />
             </div>
           </a>
         </li>
         <li>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/h.charles_1" target="_blank" rel="noopener noreferrer">
             <div className="bg-gradient-to-r from-[#feda75] via-[#fa7e1e] to-[#d62976] rounded-full p-2">
               <FaInstagram className="text-white text-xl" />
             </div>
           </a>
         </li>
         <li>
-          <a href="mailto:your-email@gmail.com" target="_blank" rel="noopener noreferrer">
+          <a href="mailto:hatumcharles1@gmail.com" target="_blank" rel="noopener noreferrer">
             <div className="bg-[#d93025] rounded-full p-2">
               <FaEnvelope className="text-white text-xl" />
             </div>
           </a>
         </li>
         <li>
-          <a href="https://wa.me/your-phone-number" target="_blank" rel="noopener noreferrer">
+          <a href="https://wa.me/+250793234963" target="_blank" rel="noopener noreferrer">
             <div className="bg-[#25d366] rounded-full p-2">
               <FaWhatsapp className="text-white text-xl" />
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/htm_charles" target="_blank" rel="noopener noreferrer">
+            <div className="bg-[#00acee] rounded-full p-2">
+              <FaTwitter className="text-white text-xl" />
             </div>
           </a>
         </li>
