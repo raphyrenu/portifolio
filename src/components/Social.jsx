1352ebb2779f990ad92ee1ad694e36d77f51ba65
Raphy <raphyboy159@gmail.com>
2025-01-13 14:59:01 +0200

    seo

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,7 +2,7 @@ import { FaTwitter, FaLinkedin, FaInstagram, FaGithub, FaEnvelope, FaWhatsapp } 
 
 const SocialMedia = () => {
   return (
-    <div className="fixed top-3/4 left-10 transform -translate-y-1/2">
+    <div className="fixed top-3/4 z-50 left-10 transform -translate-y-1/2">
       <ul className="space-y-3">
         <li>
           <a href="https://github.com/raphyrenu" target="_blank" rel="noopener noreferrer">
